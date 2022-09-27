30e6fbe2b62251dfaa292845789dd9f95189f23a
axlengblom <62358275+axlengblom@users.noreply.github.com>
2022-09-27 22:05:29 +0200

    Started work on displaying monsters via fetch

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,7 @@
   <div class="header"><h2>Dungeon Raider</h2></div>
   <Login />
 
-  <Game :room="roomInfo.room[0].name">
-    <p>{{ roomInfo.room[0].description }}</p>
-    <h2>Monster</h2>
-    <h3>{{ roomInfo.room[0].monster.monsterName }}</h3>
-    <p>Weakness : {{ roomInfo.room[0].monster.monsterWeakness }}</p>
-    <h2>Actions</h2>
-    <p>This will be all the actions available in the room</p>
-  </Game>
+  <Game> </Game>
 </template>
 
 <script>
@@ -26,28 +19,7 @@ export default {
   },
 
   data() {
-    return {
-      roomInfo: {
-        room: [
-          {
-            name: 'Room 1',
-            description:
-              'This room is filled with water, you are on a boat and see something slimy make its way up the bow',
-            monster: {
-              monsterName: 'Chutulu',
-              monsterWeakness: 'Air',
-            },
-          },
-          {
-            name: 'Room 2',
-            monster: {
-              monsterName: 'Godzilla',
-              monsterWeakness: 'None',
-            },
-          },
-        ],
-      },
-    };
+    return {};
   },
 };
 </script>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,31 @@
 <template>
-  <h1>Welcome to {{ room }}</h1>
-  <slot></slot>
+  <div v-for="monster in monsters" :key="monster.index">
+    <h1>{{ monster.name }}</h1>
+    <button @click="showUrl(monster.url)">More Info</button>
+  </div>
 </template>
 
 <script>
 export default {
-  props: { room: String },
+  data() {
+    return {
+      monsters: [],
+      currentMonster: [],
+    };
+  },
+  methods: {
+    showUrl(monsterUrl) {
+      fetch('https://www.dnd5eapi.co' + monsterUrl)
+        .then((res) => res.json())
+        .then((data) => (this.currentMonster = data));
+      console.log(this.currentMonster);
+    },
+  },
+  mounted() {
+    fetch('https://www.dnd5eapi.co/api/monsters')
+      .then((response) => response.json())
+      .then((data) => (this.monsters = data.results));
+  },
 };
 </script>
 
